48c97349880199069e224c22146142a2b102c74b
cascateer <cascateer@gmail.com>
2026-05-21 19:48:01 +0200

    rolldown

--- vite.config.ts
+++ vite.config.ts
@@ -21,4 +21,10 @@ export default {
       localsConvention: "camelCaseOnly",
     },
   },
+
+  build: {
+    rollupOptions: {
+      external: ["/node_modules/@cascateer/core/src/serializable.ts"],
+    },
+  },
 } satisfies UserConfig;
